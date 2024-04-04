d214d8f74a5636eeb9290a0fae06b060daab0ae3
John Källström <johnkaellstrom@gmail.com>
2024-04-04 19:14:23 +0200

    Create, add and mark todo

--- TodoList/app.js
+++ TodoList/app.js
@@ -2,15 +2,26 @@ const input = document.querySelector('#input');
 const button = document.querySelector('#button');
 const output = document.querySelector('#output');
 
+mark = (e) => {
+  console.log(e.target.classList.toggle('text-decoration-line-through'));
+};
+
+createTodo = (value) => {
+  const anchor = document.createElement('a');
+  anchor.setAttribute('href', '#');
+  anchor.classList.add('list-group-item');
+  anchor.classList.add('list-group-item-action');
+  anchor.innerText = value;
+  anchor.addEventListener('click', mark);
+  return anchor;
+};
+
 addTodo = () => {
-  const value = input.value;
-  if (value !== undefined && value !== '') {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.innerText = value;
-    output.appendChild(li);
+  if (input.value !== undefined && input.value !== '') {
+    const todo = createTodo(input.value);
+    output.appendChild(todo);
   } else {
-    console.log('enter something');
+    console.log('Invalid input');
   }
 };
 
